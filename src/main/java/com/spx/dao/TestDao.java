bc13615b855495f96ce0a54c95eb647370b63b3a
石鹏翔 <151105310+userspx404@users.noreply.github.com>
2024-11-01 21:55:01 +0800

    第一次测试

---
+++ src/main/java/com/spx/dao/TestDao.java
@@ -0,0 +1,5 @@
+package com.spx.dao;
+
+public interface TestDao {
+    public void sayHello();
+}
---
+++ src/main/java/com/spx/dao/impl/TestDaoImpl.java
@@ -0,0 +1,11 @@
+package com.spx.dao.impl;
+
+import com.spx.dao.TestDao;
+
+public class TestDaoImpl  implements TestDao {
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello,Study hard");
+    }
+}
